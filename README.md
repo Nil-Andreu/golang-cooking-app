0cad0dd8605d6e00a16c485b6ee6337a98682770
Nil-Andreu <nil.andreu@veepee.com>
2022-07-14 11:27:47 +0200

    Feat: Search By Tag

--- README.md
+++ README.md
@@ -80,3 +80,91 @@ And not only reading, but we might want to also write to this file each time we 
 So in each post request, we are storing the recipes data structure in the recipes.json file (which is read when we initialize the app).
 
 ## Updating Data
+For updating the recipe, we use the PUT method and we pass in the url the id of the recipe we want to update.
+Then, if this id is on one of the recipes, we replace that with the body of the request.
+```
+    // To update the Recipe
+    func UpdateRecipeHandler(c *gin.Context) {
+        // Obtain first which is the parameter
+        id := c.Param("id")
+
+        // Initialize the recipe
+        var recipe Recipe
+
+        // Bind the recipe of theb ody of the request to json
+        if err := c.ShouldBindJSON(&recipe); err != nil {
+            c.JSON(http.StatusBadRequest, gin.H{
+                "error": err.Error()})
+            return
+        }
+
+        // In the case that we have not provided with an id
+        if recipe.ID == "" {
+            recipe.ID = xid.New().String()
+        }
+
+        // We obtain the recipe we looked for
+        index := -1
+        for i := 0; i < len(recipes); i++ {
+            if recipes[i].ID == id {
+                index = i
+            }
+        }
+
+        // In the case we did not found it
+        if index == -1 {
+            response := fmt.Sprintf("Index %s not found", id)
+            c.JSON(http.StatusBadRequest, gin.H{
+                "error": response,
+            })
+        }
+
+        // If we found it, replace the recipe at that position
+        recipes[index] = recipe
+
+        // And we now need to re store it on the json file
+        file, _ := json.MarshalIndent(recipes, "", "") // Write recipes as json structure in bytes
+        _ = ioutil.WriteFile("recipes.json", file, 0644)
+
+        c.JSON(http.StatusOK, recipe)
+    }
+```
+
+## Deletetion of the Recipe
+For the deletion, we follow the same process as the update, where we find on which positon on the recipes slice it is the one we want. And then we delete it with:
+```
+    recipes = append(recipes[:index], recipes[index+1:]...)
+```
+Where this is the Golang way to remove an item at a certain position.
+
+## Search for Tags
+Another interesting thing we might want to do is to search for recipes that have an specific tag.
+This will be a GET method, where the tag will be in the url but as *?tag=* query parameter.
+```
+    func SearchRecipeHandler(c *gin.Context) {
+        // Obtain first the query parameter
+	    Tag := c.Query("tag")
+	    resultRecipes := make([]Recipe, 0)
+
+	    for recipeIndex := 0; recipeIndex < len(recipes); recipeIndex ++ {
+            // And for this recipe, we will look for all the tags that it has
+            recipe := recipes[recipeIndex]
+
+            for tagIndex := 0; tagIndex < len(recipe.Tag); tagIndex ++ {
+                // In the case that those are equal
+                if strings.EqualFold(recipe.Tag[tagIndex], Tag) {
+                    resultRecipes = append(resultRecipes, recipe)
+                }
+            }
+
+            // In the case there is no match, handle this situation
+            if len(resultRecipes) == 0 {
+                c.JSON(http.StatusOK, gin.H {
+                    "message" : "Not found",
+                })
+            } else {
+                c.JSON(http.StatusOK, resultRecipes)
+            }
+        }
+    }
+```
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"time"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/rs/xid"
@@ -15,6 +16,7 @@ import (
 type Recipe struct {
 	ID           string    `json:"id"`
 	Name         string    `json:"name"`
+	Tag  		 []string  `json:"tag"`
 	Ingredients  []string  `json:"ingredients"`
 	Instructions []string  `json:"instructions"`
 	PublishedAt  time.Time `json:"publishedAt"`
@@ -39,7 +41,7 @@ func NewRecipeHandler(c *gin.Context) {
 	// Define a new variable which is recipe, based on Recipe object
 	var recipe Recipe
 
-	// The ShouldBindJSON convert the request body into Recipe struct and assigns unique identifier with an external package called xid
+	// The ShouldBindJSON convert the request body into Recipe struct in the recipe variable
 	if err := c.ShouldBindJSON(&recipe); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error()})
@@ -67,7 +69,7 @@ func UpdateRecipeHandler(c *gin.Context) {
 	// Initialize the recipe
 	var recipe Recipe
 
-	// Bind the recipe of theb ody of the request to json
+	// Bind the recipe of the body of the request that is formatted in json to &recipe variable
 	if err := c.ShouldBindJSON(&recipe); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"error": err.Error()})
@@ -89,9 +91,9 @@ func UpdateRecipeHandler(c *gin.Context) {
 
 	// In the case we did not found it
 	if index == -1 {
-		response := fmt.Sprintf("Index %s not found", id)
+		message := fmt.Sprintf("Index %s not found", id)
 		c.JSON(http.StatusBadRequest, gin.H{
-			"error": response,
+			"error": message,
 		})
 	}
 
@@ -105,10 +107,74 @@ func UpdateRecipeHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipe)
 }
 
+func DeleteRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+
+	// Initialize the recipe
+	var recipe Recipe
+
+	// Bind the recipe of the body of the request that is formatted in json to &recipe variable
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		message := fmt.Sprintf("Index %s not found", id)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": message,
+		})
+	}
+
+	// And now we delete the recipe at that position
+	recipes = append(recipes[:index], recipes[index+1:]...)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Deleted correctly",
+	})
+
+}
+
+func SearchRecipeHandler(c *gin.Context) {
+	Tag := c.Query("tag")
+	resultRecipes := make([]Recipe, 0)
+
+	for recipeIndex := 0; recipeIndex < len(recipes); recipeIndex ++ {
+		// And for this recipe, we will look for all the tags that it has
+		recipe := recipes[recipeIndex]
+
+		for tagIndex := 0; tagIndex < len(recipe.Tag); tagIndex ++ {
+			// In the case that those are equal
+			if strings.EqualFold(recipe.Tag[tagIndex], Tag) {
+				resultRecipes = append(resultRecipes, recipe)
+			}
+		}
+
+		// In the case there is no match, handle this situation
+		if len(resultRecipes) == 0 {
+			c.JSON(http.StatusOK, gin.H {
+				"message" : "Not found",
+			})
+		} else {
+			c.JSON(http.StatusOK, resultRecipes)
+		}
+	}
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/recipes", GetSliceRecipes)
 	router.POST("/recipes", NewRecipeHandler)
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
+	router.DELETE("/recipes/:id", DeleteRecipeHandler)
+	router.GET("/recipes/search", SearchRecipeHandler)
 	router.Run(":6000")
 }
